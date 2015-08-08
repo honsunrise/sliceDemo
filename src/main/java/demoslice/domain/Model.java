e13ec46ba130fd39772a621acbf01ee02e9bd13b
iCoderc <zhsyourai@163.com>
2015-08-08 12:12:02 +0800

    I am foolish

--- src/main/java/demoslice/domain/Model.java
+++ src/main/java/demoslice/domain/Model.java
@@ -6,7 +6,6 @@ import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
-import java.util.Objects;
 
 /**
  * Created by iCoderc on 2015/8/8.
