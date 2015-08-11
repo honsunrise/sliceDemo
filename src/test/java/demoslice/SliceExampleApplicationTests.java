d1febbf8ca49f78ce94d0c0a777ab830727eeccf
iCoderc <zhsyourai@163.com>
2015-08-11 09:53:12 +0800

    Write test

--- src/test/java/demoslice/SliceExampleApplicationTests.java
+++ src/test/java/demoslice/SliceExampleApplicationTests.java
@@ -1,18 +1,38 @@
 package demoslice;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = SliceExampleApplication.class)
 @WebAppConfiguration
 public class SliceExampleApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+	@Autowired
+	private WebApplicationContext context;
+	private MockMvc mockMvc;
 
+	@Before
+	public void setUp() {
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(this.context)
+				.dispatchOptions(true)
+				.build();
+	}
+	@Test
+	public void contextLoads() throws Exception {
+		this.mockMvc.perform(get("/otherModelList")).andExpect(status().isOk());
+	}
 }
